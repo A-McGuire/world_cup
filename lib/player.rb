b12afe824581e798a449d111b4ca63bc892ae7cf
Aidan McGuire <>
2021-02-16 17:04:32 -0700

    Iteration one

---
+++ lib/player.rb
@@ -0,0 +1,14 @@
+class Player
+  attr_reader :name_position
+  def initialize(name_position)
+    @name_position = name_position
+  end
+
+  def name
+    name_position[:name]
+  end
+
+  def position
+    name_position[:position]
+  end
+end
